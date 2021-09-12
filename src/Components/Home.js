7280f669215c4f33248267057a018720af5580c7
praveenvjpm <praveenvjpm@gmail.com>
2021-09-12 14:00:05 +0530

    Redirect

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import Footer from './Footer'
 import Header from './Header'
+import { Redirect } from 'react-router'
 
 export const Home = () => {
+
+    const [login,setLogin] = useState(false);
+    if(login){
+        return <Redirect to='/dashboard' />
+    }
     return (
         <div>
             <Header />
             <h3>This is Home</h3>
+            <button onClick={()=>setLogin(true)} >Login</button>
             <Footer />
         </div>
     )
